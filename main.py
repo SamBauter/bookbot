e0c47fbd76a4bc8899b9d3b547c62e78305939ab
samuelbauter <samuelbauter@gmail.com>
2025-02-02 21:10:58 -0700

    L9 complete

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def main(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     print(file_contents)
+    print(count_words(file_contents))
 
+
+def count_words(book_string):
+    return len(book_string.split())
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
