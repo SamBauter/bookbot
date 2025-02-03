f3156ccaedcd955fe55a9435a506d12d9978b965
samuelbauter <samuelbauter@gmail.com>
2025-02-02 20:28:24 -0700

    Made initial function main

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+def main(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    print(file_contents)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
