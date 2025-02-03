9e3154b00abf41967fea42c8f19f8006b11ffc9d
samuelbauter <samuelbauter@gmail.com>
2025-02-02 21:15:17 -0700

    L10 complete

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
+from collections import Counter
+
 def main(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     print(file_contents)
     print(count_words(file_contents))
+    print(count_chars(file_contents))
+
+def count_chars(book_string):
+    book_string_lower = book_string.lower()
+    return dict(Counter(book_string_lower))
 
 
 def count_words(book_string):
