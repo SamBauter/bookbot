b4384bc362a08d64a9c02fba034e865bc2621044
samuelbauter <samuelbauter@gmail.com>
2025-02-02 21:25:23 -0700

    L11 complete

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 from collections import Counter
+import string
 
 def main(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-    print(file_contents)
-    print(count_words(file_contents))
-    print(count_chars(file_contents))
+    #print(file_contents)
+    print(f"---Begin report of {path_to_file} ---")
+    print(f"{count_words(file_contents)} found in the document")
+    char_count_dict = count_chars(file_contents)
+    filtered_char_count_dict = {char:count for char,count in char_count_dict.items() if char in list(string.ascii_lowercase) }
+    for char,count in filtered_char_count_dict.items():
+        print(f"The '{char}' character was found {count} times")
 
 def count_chars(book_string):
     book_string_lower = book_string.lower()
